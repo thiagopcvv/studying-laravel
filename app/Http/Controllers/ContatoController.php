16338c21a50429b3d7cea7015901c0396a276697
thiagopcvv <thiagopiresdovalle@hotmail.com>
2024-06-14 15:02:32 -0400

    adicionando validação para formulário

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,20 +8,28 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        
+    public function contato()
+    {
+
         $motivos = MotivoContato::all();
         return view('site.contato', ['motivos' => $motivos]);
     }
 
-    public function salvar(Request $request){
-        $request->validate([
-            'nome' => 'required', 
-            'telefone' => 'required',
-            'email' => 'email',
-            'mensagem' => 'required',
-            "motivo_contatos_id" => 'required'   
-        ]);
+    public function salvar(Request $request)
+    {
+        $request->validate(
+            [
+                'nome' => 'required|unique:site_contatos',
+                'telefone' => 'required',
+                'email' => 'email',
+                'mensagem' => 'required',
+                "motivo_contatos_id" => 'required'
+            ],
+            [
+                'email.email' => 'O email precisa ser válido',
+                'required' => 'Esse campo precisa ser preenchido',
+            ]
+        );
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -3,10 +3,19 @@
 <form action={{ route('site.salvar') }} method="post">
     @csrf
     <input name="nome" type="text" placeholder="Nome" class="borda-{{ $class }}">
+    @if ($errors->has('nome'))
+        <a style="font-weight: bold; color: rgb(212, 27, 27)"> *{{ $errors->first('nome') }}</a>
+    @endif
     <br>
     <input name="telefone" type="text" placeholder="Telefone" class="borda-{{ $class }}">
+    @if ($errors->has('telefone'))
+        <a style="font-weight: bold; color: rgb(212, 27, 27)"> *{{ $errors->first('telefone') }}</a>
+    @endif
     <br>
     <input name="email" type="text" placeholder="E-mail" class="borda-{{ $class }}">
+    @if ($errors->has('email'))
+        <a style="font-weight: bold; color: rgb(212, 27, 27)"> *{{ $errors->first('email') }}</a>
+    @endif
     <br>
     <select name="motivo_contatos_id" class="borda-{{ $class }}">
         <option value="">Qual o motivo do contato?</option>
@@ -14,11 +23,15 @@
             <option value="{{ $motivo->id }}">{{ $motivo->motivo }}</option>
         @endforeach
     </select>
+    @if ($errors->has('motivo_contatos_id'))
+        <a style="font-weight: bold; color: rgb(212, 27, 27)"> *{{ $errors->first('motivo_contatos_id') }}</a>
+    @endif
     <br>
     <textarea name="mensagem" class="borda-{{ $class }}" placeholder="Preencha aqui a sua mensagem"></textarea>
+    @if ($errors->has('mensagem'))
+        <a style="font-weight: bold; color: rgb(212, 27, 27)"> *{{ $errors->first('mensagem') }}</a>
+    @endif
     <br>
     <button type="submit" class="borda-{{ $class }}">ENVIAR</button>
-    <pre style="background: rgb(196, 100, 100)">
-        {{ print_r($errors) }}
-    </pre>
+
 </form>
