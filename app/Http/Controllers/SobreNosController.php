cfad0b45b1763ad072c664ec2779b645beb45b1d
thiagopcvv <thiagopiresdovalle@hotmail.com>
2024-06-17 11:44:23 -0400

    middleware no controller

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\Middleware;
 
 class SobreNosController extends Controller
 {
+
+    public static function middleware()
+    {
+        return [
+            new Middleware(LogAcessoMiddleware::class)
+        ];
+    }
+
     public function sobreNos() {
         return view('site.sobre-nos');
     }
